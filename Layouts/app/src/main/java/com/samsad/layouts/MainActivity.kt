93397ae5036bb8010fd5b7bb0c305bf9aed2b26d
samsadch <samsadch@gmail.com>
2020-09-02 23:16:11 +0400

    Color my View added

--- Layouts/app/src/main/java/com/samsad/layouts/MainActivity.kt
+++ Layouts/app/src/main/java/com/samsad/layouts/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.samsad.layouts
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.samsad.layouts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var myname: MyName = MyName("Samsad CV")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myname
+    }
+
+    fun addNickName(view: View) {
+        binding.apply {
+            //binding.nicknameText.text = binding.nicknameEdit.text
+            myname.nickname = binding.nicknameEdit.text.toString()
+            // Invalidate all binding expressions and request a new rebind to refresh UI
+            invalidateAll()
+            binding.nicknameEdit.visibility = View.GONE
+            view.visibility = View.GONE
+            binding.nicknameText.visibility = View.VISIBLE
+        }
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
