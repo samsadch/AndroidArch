d6ca2619716bca29a6095f6dbe5533868b6fecc8
Samsad CV <samsadch@gmail.com>
2020-11-01 16:47:04 +0400

    Deleted extra files

---
+++ MarsNetwork/app/src/main/java/com/samsad/kotlinmarsnetwork/network/MarsApiService.kt
@@ -0,0 +1,3 @@
+package com.samsad.kotlinmarsnetwork.network
+
+private const val BASE_URL = "https://mars.udacity.com/"
---
+++ MarsNetwork/app/src/main/java/com/samsad/kotlinmarsnetwork/network/MarsProperty.kt
@@ -0,0 +1,3 @@
+package com.samsad.kotlinmarsnetwork.network
+
+class MarsProperty()
