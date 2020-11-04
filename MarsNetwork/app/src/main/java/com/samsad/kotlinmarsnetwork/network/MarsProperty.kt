8a9175f7e2c6e238566cdb9291cb29768bdd7efd
Samsad CV <samsadch@gmail.com>
2020-11-04 16:22:57 +0400

    Mars property parcelizes

--- MarsNetwork/app/src/main/java/com/samsad/kotlinmarsnetwork/network/MarsProperty.kt
+++ MarsNetwork/app/src/main/java/com/samsad/kotlinmarsnetwork/network/MarsProperty.kt
@@ -1,10 +1,13 @@
 package com.samsad.kotlinmarsnetwork.network
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class MarsProperty(
     val id: String,
     @Json(name = "img_src") val imageSourceUrl: String,
     val type: String,
     val price: Double
-)
+) : Parcelable
