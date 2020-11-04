cca958c24c52c2ca3582f5231185bec8d7788539
Samsad CV <samsadch@gmail.com>
2020-11-04 17:15:31 +0400

    Detail fragment updated with viewmodel

--- MarsNetwork/app/src/main/java/com/samsad/kotlinmarsnetwork/detail/DetailViewModel.kt
+++ MarsNetwork/app/src/main/java/com/samsad/kotlinmarsnetwork/detail/DetailViewModel.kt
@@ -2,12 +2,23 @@ package com.samsad.kotlinmarsnetwork.detail
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.samsad.kotlinmarsnetwork.network.MarsProperty
 
 /**
  * The [ViewModel] that is associated with the [DetailFragment].
  */
-class DetailViewModel(@Suppress("UNUSED_PARAMETER") marsProperty: MarsProperty, app: Application) :
+class DetailViewModel(marsProperty: MarsProperty, app: Application) :
     AndroidViewModel(app) {
+
+    private val _selectedProperty = MutableLiveData<MarsProperty>()
+    val selectedProperty: LiveData<MarsProperty>
+        get() = _selectedProperty
+
+    init {
+        _selectedProperty.value = marsProperty
+    }
+
 }
