7d64f3e596f4af08b5e90bbe2f24fb3149d594e3
samsadch@gmail.com <samsadch@gmail.com>
2020-11-07 23:07:56 +0400

    TODO Removed

--- DevBiteBackground/app/src/main/java/com/samsad/devbitebackground/database/DatabaseEntities.kt
+++ DevBiteBackground/app/src/main/java/com/samsad/devbitebackground/database/DatabaseEntities.kt
@@ -4,8 +4,6 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.samsad.devbitebackground.domain.Video
 
-// TODO (01) Create the DatabaseEntities class, adding annotations for the class and the primary key.
-
 @Entity
 data class DatabaseVideo(
     @PrimaryKey
@@ -16,7 +14,6 @@ data class DatabaseVideo(
     val thumbnail: String
 )
 
-// TODO (02) Define extension function List<DatabaseVideo>.asDomainModel(), that returns
 // a list of <Video>.
 fun List<DatabaseVideo>.asDomainModel(): List<Video> {
     return map {
