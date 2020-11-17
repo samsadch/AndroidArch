283d972bee27a944e1a8aed34d9895d26df7b3ac
samsadch@gmail.com <samsadch@gmail.com>
2020-11-18 00:24:50 +0400

    Work constarints

--- DevBiteBackground/app/src/main/java/com/samsad/devbitebackground/MyApp.kt
+++ DevBiteBackground/app/src/main/java/com/samsad/devbitebackground/MyApp.kt
@@ -1,16 +1,15 @@
 package com.samsad.devbitebackground
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.ExistingWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
+import android.os.Build
+import androidx.work.*
 import com.samsad.devbitebackground.work.RefreshDataWork
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
+import kotlin.reflect.jvm.internal.impl.load.java.Constant
 
 class MyApp : Application() {
     private val applicationScope = CoroutineScope(Dispatchers.Default)
@@ -29,9 +28,22 @@ class MyApp : Application() {
     }
 
     private fun setupRecurringWork() {
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    setRequiresDeviceIdle(true)
+                }
+            }.build()
+
         val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWork>(
             1, TimeUnit.DAYS
-        ).build()
+        )
+            .setConstraints(constraints)
+            .build()
         //To schedule a work we use enqueue on work manager
         WorkManager.getInstance().enqueueUniquePeriodicWork(
             RefreshDataWork.WORK_NAME,
